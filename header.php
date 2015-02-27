782353f16bc86bddc087a4f3bc17fb31ad98d780
Robbie Antenesse <dev@alamantus.com>
2015-02-27 00:41:41 -0700

    Added link to display favicon correctly.

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
+    <link rel="shortcut icon" href="favicon.ico">
+    
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="styles/main.css">
     
