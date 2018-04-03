6700ee7ca2792d5712c5055cd0d12b4519d73e6f
Robbie Antenesse <dev@alamantus.com>
2018-04-03 10:08:10 -0600

    Remove the count from the footer.

--- footer.php
+++ footer.php
@@ -5,21 +5,6 @@
     <div id="termsLink" class="footerLink"><a href=".?page=terms">Legal</a></div>
     <div id="githubLink" class="footerLink"><a href="https://github.com/Alamantus/Checkpointer" target="_blank">GitHub</a></div>
     <div id="userCount" class="footerLink" style="font-size: 10px;">
-<?php
-    $user_count_query_sql = "SELECT id FROM user WHERE id > 1;";
-    $user_count_query = query($user_count_query_sql);
-    $user_count = 0;
-    while($user = fetch_assoc($user_count_query)) {
-        $user_count++;
-    }
-    $checkpoints_count_query_sql = "SELECT id FROM checkpoint WHERE owner > 1;";
-    $checkpoints_count_query = query($checkpoints_count_query_sql);
-    $checkpoints_count = 0;
-    while($checkpoint = fetch_assoc($checkpoints_count_query)) {
-        $checkpoints_count++;
-    }
-    echo $user_count . " checkpointers registered<br />" . $checkpoints_count . " checkpoints created";
-?>
     </div>
     <div id="copyright">&copy; 2015 <a href="http://www.alamantus.com" target="_blank">Alamantus GameDev</a></div>
 </div>
