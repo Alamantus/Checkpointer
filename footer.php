f7bf52848759751fab1a01b1721c69ed75ce2ec1
Robbie Antenesse <dev@alamantus.com>
2018-04-03 11:48:45 -0600

    Small fixes: updated copyright and links; removed og-* meta

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 </div>
 <div id="footer">
     <div id="disclaimer" style="display:inline-block; margin: 0px 15px; font-size: 70%; max-width: 250px;">This is the alpha release of Checkpointer. Please report any bugs or issues to the <strong><a href="https://github.com/Alamantus/Checkpointer/issues" target="_blank">GitHub Issue Tracker</a></strong>.</div>
-    <div id="aboutLink" class="footerLink"><a href=".?page=about">About</a></div>
-    <div id="termsLink" class="footerLink"><a href=".?page=terms">Legal</a></div>
+    <div id="aboutLink" class="footerLink"><a href="./?page=about">About</a></div>
+    <div id="termsLink" class="footerLink"><a href="./?page=terms">Legal</a></div>
     <div id="githubLink" class="footerLink"><a href="https://github.com/Alamantus/Checkpointer" target="_blank">GitHub</a></div>
     <div id="userCount" class="footerLink" style="font-size: 10px;">
     </div>
-    <div id="copyright">&copy; 2015 <a href="http://www.alamantus.com" target="_blank">Alamantus GameDev</a></div>
+    <div id="copyright">&copy; <?php echo date("Y"); ?> <a href="https://robbie.antenesse.net" target="_blank">Robbie Antenesse</a></div>
 </div>
-<script src="scripts/validation.js"></script>
-<script src="scripts/jquery_actions.js"></script>
-<script src="scripts/jquery_ui_actions.js"></script>
+<script src="./scripts/validation.js"></script>
+<script src="./scripts/jquery_actions.js"></script>
+<script src="./scripts/jquery_ui_actions.js"></script>
 </body>
 </html>
